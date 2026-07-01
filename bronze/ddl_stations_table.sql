e9084f2c7f193e857035bf9b39e3062a1de62762
Summer-pixel12 <harshhyd221@gmail.com>
2026-07-01 20:36:38 +0530

    Create ddl_stations_table.sql

---
+++ bronze/ddl_stations_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE gold.stations(
+id INT,
+station_code VARCHAR(10),
+city VARCHAR(50),
+state_code VARCHAR(3),
+pin_code INT,
+latitude FLOAT,
+longitude FLOAT,
+elevation_m FLOAT,
+topo_complexity FLOAT,
+coastal_proximity FLOAT,
+valley_factor FLOAT
+)
