67f5810dad03e5580372fa70cf256cb880912e34
Summer-pixel12 <harshhyd221@gmail.com>
2026-07-18 13:22:36 +0530

    Update insert_data_in_bronze.py

--- bronze/insert_data_in_bronze.py
+++ bronze/insert_data_in_bronze.py
@@ -1,15 +1,29 @@
+#This is the python script used to insert the data from the raw csv files to ms sql server bronze tables which we created previously
+# The connection between the sql Server and python is established using pyodbc
+# in step 1 
+# In the 2nd step the Folder path for each CSV file is stored in a list using OS and GLOB
+# ignoring the stations.csv file as it is a separate file not to be included in the bronze.data
+# table
+# In step 3 a for loop is established on the all_staion_files list which stores the file path of 
+# csv file we need to inesrt in the bronze.data table in ms sql server, we also reindex the columns 
+# so that any missing columns are filled with nan. Station_code is added at the start of every row to keep
+# track of the source of the readings
+# The last thing done in the loop is to execute the bulk insert query to insert the data in the 
+# required table
+
+
 import pandas as pd
 import glob
 import os
-import pyodbc
+import pyodbc  
 
 # ==========================================
 # 1. DATABASE CONFIGURATION (MS SQL SERVER)
 # ==========================================
 conn_str = (
     r'DRIVER={ODBC Driver 17 for SQL Server};'
-    r'SERVER=localhost\SQLEXPRESS;' 
-    r'DATABASE=aqi_report;'   
+    r'SERVER=localhost\SQLEXPRESS;' # The name of your server
+    r'DATABASE=aqi_report;'         # The name of your DB
     r'Trusted_Connection=yes;'
 )
 
@@ -56,7 +70,7 @@ for i, filename in enumerate(all_station_files):
     # Convert to a list of tuples
     raw_records = [tuple(x) for x in df.to_numpy()]
     
-    # 🌟 CRITICAL FIX: Explicitly cast every non-null value to a standard string primitive.
+    # CRITICAL FIX: Explicitly cast every non-null value to a standard string primitive.
     # This strips away any underlying numpy float behaviors that trick the ODBC driver.
     records = []
     for row in raw_records:
@@ -70,7 +84,8 @@ for i, filename in enumerate(all_station_files):
     cursor.fast_executemany = True
     cursor.executemany(query, records)
     conn.commit()
-    
+
+    #Print the process on the screen for every 5 files inserted
     if (i + 1) % 5 == 0:
         print(f"Progress: Successfully staged {i + 1} / {len(all_station_files)} files.")
 
