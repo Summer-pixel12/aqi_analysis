302fb1414d4982141a109553ef4d4219c640838d
Summer-pixel12 <harshhyd221@gmail.com>
2026-07-18 13:32:00 +0530

    Update insert_stations_silver.sql

--- silver/insert_stations_silver.sql
+++ silver/insert_stations_silver.sql
@@ -1,3 +1,5 @@
+-- This query inserts the data from bronze.data to silver.data 
+-- It discards the data of the unknown stations
 INSERT INTO silver.stations(
 id,station_code,city,state_code,pin_code,latitude,longitude,elevation_m,topo_complexity,coastal_proximity,valley_factor
 )
